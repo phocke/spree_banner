90211ef3472474773a9ac5c9eb0dd50a1a6bbc33
Przemek <phocke@gmail.com>
2013-04-16 09:58:58 +0400

    Banner model visible scope

--- app/models/spree/banner.rb
+++ app/models/spree/banner.rb
@@ -1,5 +1,6 @@
 class Spree::Banner < ActiveRecord::Base
-  attr_accessible :link, :name, :visible
-  
+  attr_accessible :visible, :name, :link
   has_many :images, :as => :viewable, :order => :position, :dependent => :destroy
+
+  scope :visible, where(:visible => true)
 end
