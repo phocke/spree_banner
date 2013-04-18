bac7afcc550dc7fde250c83f8e50655930a95c69
Przemek <phocke@gmail.com>
2013-04-18 09:50:38 +0400

    Banner views finished

--- app/controllers/spree/admin/banners_controller.rb
+++ app/controllers/spree/admin/banners_controller.rb
@@ -3,4 +3,10 @@ class Spree::Admin::BannersController < Spree::Admin::ResourceController
   def index
     @banners = Spree::Banner.all
   end
+
+  def new
+    @banner = Spree::Banner.new
+  end
+
+
 end
