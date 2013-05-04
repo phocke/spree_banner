4f4eadfe9cf9135bc267ca236a74fd9808ca73e8
Przemek <phocke@gmail.com>
2013-05-04 23:46:17 +0400

    Add image to banners

--- app/models/spree/banner.rb
+++ app/models/spree/banner.rb
@@ -1,6 +1,17 @@
 class Spree::Banner < ActiveRecord::Base
-  attr_accessible :visible, :name, :link
-  has_many :images, :as => :viewable, :order => :position, :dependent => :destroy
+  attr_accessible :visible, :name, :link, :image
 
-  scope :visible, where(:visible => true)
+  validates_presence_of :image, :name, :link
+  has_attached_file :image,
+    :styles => { :normal => '590x170' },
+    :default_style => :normal,
+    :url => '/spree/banners/:id/:style/:basename.:extension',
+    :path => ':rails_root/public/spree/banners/:id/:style/:basename.:extension',
+    :default_url => '/assets/default_banner.png'
+
+  include Spree::Core::S3Support
+  supports_s3 :image
+
+  scope :visible, where("image_file_name IS NOT NULL AND visible = true")
+
 end
